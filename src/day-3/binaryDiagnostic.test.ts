6382b9ac0514fce3fa187b3aa9b813d5bcdae7af
Brian Azizi <brian.azizi@cazoo.co.uk>
2021-12-19 18:51:47 +0000

    Day 3 Complete

--- src/day-3/binaryDiagnostic.test.ts
+++ src/day-3/binaryDiagnostic.test.ts
@@ -1,7 +1,9 @@
 import {
   getDiagnosticReport,
-  getGammaAndEpsilon,
+  renderGammaAndEpsilon,
+  getLifeSupport,
   getNumberOfBits,
+  getOxygenAndCo2,
   getPowerConsumption,
 } from "./binaryDiagnostic";
 
@@ -9,14 +11,27 @@ describe("Binary Diagnostic", () => {
   describe("Outside", () => {
     test("Example 1", () => {
       const diagnosticReport = getDiagnosticReport("./day-3/example.txt");
-      const [gamma, epsilon] = getGammaAndEpsilon(diagnosticReport);
+      const [gamma, epsilon] = renderGammaAndEpsilon(diagnosticReport);
       expect(getPowerConsumption(gamma, epsilon)).toBe(198);
+    });
+
+    test("Example 2", () => {
+      const diagnosticReport = getDiagnosticReport("./day-3/example.txt");
+      const [oxygen, co2] = getOxygenAndCo2(diagnosticReport);
+      expect([oxygen, co2]).toStrictEqual([0b10111, 0b01010]);
+      expect(getLifeSupport(oxygen, co2)).toBe(230);
     });
 
     test("Puzzle 1", () => {
       const diagnosticReport = getDiagnosticReport("./day-3/input.txt");
-      const [gamma, epsilon] = getGammaAndEpsilon(diagnosticReport);
+      const [gamma, epsilon] = renderGammaAndEpsilon(diagnosticReport);
       expect(getPowerConsumption(gamma, epsilon)).toBe(4139586);
+    });
+
+    test("Puzzle 2", () => {
+      const diagnosticReport = getDiagnosticReport("./day-3/input.txt");
+      const [oxygen, co2] = getOxygenAndCo2(diagnosticReport);
+      expect(getLifeSupport(oxygen, co2)).toBe(1800151);
     });
   });
 
@@ -40,8 +55,8 @@ describe("Binary Diagnostic", () => {
 
     test.each([
       [getDiagnosticReport("./day-3/example.txt"), [0b10110, 0b01001]],
-    ])("getGammaAndEpsilon", (report, gammaAndEpsilon) => {
-      expect(getGammaAndEpsilon(report)).toStrictEqual(gammaAndEpsilon);
+    ])("renderGammaAndEpsilon", (report, gammaAndEpsilon) => {
+      expect(renderGammaAndEpsilon(report)).toStrictEqual(gammaAndEpsilon);
     });
 
     test.each([
--- src/day-3/binaryDiagnostic.ts
+++ src/day-3/binaryDiagnostic.ts
@@ -22,22 +22,49 @@ export function getNumberOfBits(diagnosticReport: DiagnosticReport): number {
   return diagnosticReport[0].length;
 }
 
-export function getGammaAndEpsilon(
-  diagnosticReport: DiagnosticReport
-): [Gamma, Epsilon] {
+function getGammaAndEpsilon(diagnosticReport: number[][]) {
   const numberOfBits = getNumberOfBits(diagnosticReport);
   const gamma = Array(numberOfBits).fill(0);
   const epsilon = Array(numberOfBits).fill(0);
   for (let i = 0; i < numberOfBits; i++) {
-    [gamma[i], epsilon[i]] =
-      diagnosticReport
-        .map((numArray) => numArray[i] || -1)
-        .reduce((acc, n) => acc + n, 0) >= 0
-        ? [1, 0]
-        : [0, 1];
+    [gamma[i], epsilon[i]] = isBitCommonlyOn(diagnosticReport, i)
+      ? [1, 0]
+      : [0, 1];
   }
+  return { gamma, epsilon };
+}
+
+export function renderGammaAndEpsilon(
+  diagnosticReport: DiagnosticReport
+): [Gamma, Epsilon] {
+  const { gamma, epsilon } = getGammaAndEpsilon(diagnosticReport);
 
   return [renderBinary(gamma), renderBinary(epsilon)];
+}
+
+function isBitCommonlyOn(oxygen: number[][], i: number) {
+  return (
+    oxygen
+      .map((numArray) => numArray[i] || -1)
+      .reduce((acc, n) => acc + n, 0) >= 0
+  );
+}
+
+export function getOxygenAndCo2(
+  diagnosticReport: DiagnosticReport
+): [number, number] {
+  let oxygen = diagnosticReport;
+  for (let i = 0; oxygen.length > 1; i++) {
+    const commonBit = isBitCommonlyOn(oxygen, i) ? 1 : 0;
+    oxygen = oxygen.filter((num) => num[i] === commonBit);
+  }
+
+  let co2 = diagnosticReport;
+  for (let i = 0; co2.length > 1; i++) {
+    const uncommonBit = isBitCommonlyOn(co2, i) ? 0 : 1;
+    co2 = co2.filter((num) => num[i] === uncommonBit);
+  }
+  return [renderBinary(oxygen[0]), renderBinary(co2[0])];
 }
 
 export function renderBinary(binaryArray: number[]) {
@@ -49,4 +76,8 @@ export function renderBinary(binaryArray: number[]) {
 
 export function getPowerConsumption(gamma: number, epsilon: number) {
   return gamma * epsilon;
+}
+
+export function getLifeSupport(oxygen: number, co2: number) {
+  return oxygen * co2;
 }
